bcc22fc8003ac596989b26b0470ff039bbd0ddc0
zagahr <zagahr.mail@googlemail.com>
2019-03-23 19:05:55 +0000

    Inital commit

---
+++ Conferences/Styleguide.swift
@@ -0,0 +1,70 @@
+//
+//  Styleguide.swift
+//  Conferences
+//
+//  Created by Zagahr on 23/03/2019.
+//  Copyright © 2019 Timon Blask. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIColor {
+
+    static var primaryText: UIColor {
+        return UIColor(white: 0.9, alpha: 1.0)
+    }
+
+    static var secondaryText: UIColor {
+        return UIColor(white: 0.75, alpha: 1.0)
+    }
+
+    static var tertiaryText: UIColor {
+        return UIColor(white: 0.55, alpha: 1.0)
+    }
+
+    static var panelBackground: UIColor {
+        return UIColor(red:0.13, green:0.13, blue:0.13, alpha:1.0)
+    }
+
+    static var darkWindowBackground: UIColor {
+        return UIColor(red:0.12, green:0.12, blue:0.12, alpha:1.0)
+    }
+
+    static var elementBackground: UIColor {
+        return UIColor(red:0.09, green:0.09, blue:0.09, alpha:1.0)
+    }
+
+    static var inactiveButton: UIColor {
+        return UIColor(red:0.09, green:0.09, blue:0.09, alpha:1.0)
+    }
+
+    static var activeButton: UIColor {
+        return UIColor(red:0.11, green:0.11, blue:0.11, alpha:1.0)
+    }
+
+    static var prefsPrimaryText: UIColor {
+        return UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 1.00)
+    }
+
+    static var prefsSecondaryText: UIColor {
+        return UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.00)
+    }
+
+    static var prefsTertiaryText: UIColor {
+        return UIColor(red: 0.49, green: 0.49, blue: 0.49, alpha: 1.00)
+    }
+
+    static var errorText: UIColor {
+        return UIColor(red: 0.85, green: 0.18, blue: 0.18, alpha: 1.00)
+    }
+
+//    static var inactiveColor: UIColor {
+//        return UIColor.init(hexString: "B3B3B3")
+//    }
+
+    static var activeColor: UIColor {
+        return .white
+    }
+
+}
